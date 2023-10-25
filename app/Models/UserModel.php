3fdddf47d01f156cde9d0c19541e88e2adee43f7
Yokool <letsgamecze@gmail.com>
2023-10-25 19:14:40 +0200

    first draft

---
+++ app/Models/UserModel.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+
+final class UserModel
+{
+
+    public function __construct(
+        private Nette\Database\Explorer $databaseExplorer
+    ) {
+        
+    }
+
+    public function insertNewMember(
+        $first_name,
+        $last_name
+        )
+    {
+        $this->databaseExplorer
+            ->table('team_member')
+            ->insert([
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+            ]);
+    }
+    
+}
--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -5,8 +5,34 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
-
+use Nette\Application\UI\Form;
+use App\Models\UserModel;
 
 final class HomePresenter extends Nette\Application\UI\Presenter
 {
+
+    public function __construct(
+        private UserModel $memberModel
+    ) {
+
+    }
+
+    public function createComponentMemberAddForm(): Form
+    {
+        $form = new Form;
+        $form->addText('first_name')->setRequired('Please fill in your first name.');
+        $form->addText('last_name')->setRequired('Please fill in your last name.');
+        
+        $form->addSubmit('add_user');
+        $form->onSuccess[] = [$this, 'memberAddFormSucceeded'];
+        return $form;
+    }
+
+    public function memberAddFormSucceeded(Form $form, $data)
+    {
+        $this->memberModel->insertNewMember(
+            $data['name']
+        );
+    }
+
 }
